d4c4062d0c6d482fcfd696128c166405e3941e63
LIUYONG <ljy123liu@sina.com>
2017-10-07 14:04:52 +0800

    三方库

--- MyApp/Modules/Home/detail.js
+++ MyApp/Modules/Home/detail.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View,Button, TouchableHighlight } from "react-native";
+import Swiper from "react-native-swiper"
+import { StyleSheet, Text, View,Button, Image,TouchableHighlight,ActivityIndicator,Dimensions } from "react-native";
 
 export default class Detail extends React.Component {
 
@@ -7,14 +8,38 @@ export default class Detail extends React.Component {
         title: `Chat with ${navigation.state.params.user}`,
     });
 
+    constructor(props) {
+        super(props);
+        this.state = {
+
+            advertisements: [
+                {title:'广告1',backgroundColor:'gray',url:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507226069759&di=851adcf8a30dfe9e746776d11efd554b&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F14%2F87%2F22%2F40858PIC4AX_1024.jpg'},
+                {title:'广告2',backgroundColor:'yellow',url:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507226069759&di=851adcf8a30dfe9e746776d11efd554b&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F14%2F87%2F22%2F40858PIC4AX_1024.jpg'},
+                {title:'广告3',backgroundColor:'red',url:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507226069759&di=851adcf8a30dfe9e746776d11efd554b&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F14%2F87%2F22%2F40858PIC4AX_1024.jpg'},
+            ]
+        };
+    }
+
     render() {
 
         const { params } = this.props.navigation.state;
 
         return (
             <View>
+                <View style={styles.advertisement}>
+                    <Swiper loop={true} autoplay={true}>
+                        {this.state.advertisements.map((advertisement, index) => {
+                            return (
+                                <TouchableHighlight key={index} onPress={this.bannerClick}>
+                                    <Image style={styles.advertisement} source={{uri:advertisement.url}}></Image>
+                                </TouchableHighlight>
+                            )
+                        })}
+                    </Swiper>
+                </View>
                 <Text>Chat with {params.user}</Text>
                 <Button onPress={this.back} title='返回'></Button>
+                <ActivityIndicator color='pink' size='large'></ActivityIndicator>
             </View>
         );
     }
@@ -24,3 +49,10 @@ export default class Detail extends React.Component {
         this.props.navigation.state.params.callback('回调参数');
     }
 }
+
+const styles = StyleSheet.create({
+    advertisement: {
+        height: 180,
+        width: Dimensions.get('window').width
+    },
+})
